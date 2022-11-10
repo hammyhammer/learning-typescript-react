bd476825b9a0359413d63dc2f00b668eee650faa
hammyhammer <hammyjacobs@gmail.com>
2022-11-10 10:02:09 -0500

    notes about .FC

--- shopping-list/src/components/Greeter.tsx
+++ shopping-list/src/components/Greeter.tsx
@@ -5,3 +5,17 @@ export default function Greeter(): JSX.Element {
     <h1>Hello There</h1>
   )
 }
+
+// const Greeter: React.FC = () => {
+//   return <h1>Hello There x2aå</h1>
+// }
+
+// export default Greeter
+
+// Both of the examples above are valid. 
+// .FC is discouraged though. You may see it in other
+// code bases. It is an older way of writing a component 
+// The first example is the syntax that create-react-app uses
+// nowadays.
+
+
