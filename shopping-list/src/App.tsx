b9f63b7854741e0db8951afeec2e7437e5ec6182
hammyhammer <hammyjacobs@gmail.com>
2022-11-10 10:43:01 -0500

    more robust props and interfaces

--- shopping-list/src/App.tsx
+++ shopping-list/src/App.tsx
@@ -5,10 +5,14 @@ import Greeter from './components/Greeter';
 import ShoppingList from './components/ShoppingList';
 
 function App() {
+  const items = [
+    { id: 1, product: "Lemon", quantity: 3 },
+    { id: 2, product: "Chicken", quantity: 2 },
+  ];
   return (
     <div>
       {/* <Greeter person="Bobby" /> */}
-      <ShoppingList />
+      <ShoppingList items={items} />
     </div>
   );
 }
--- shopping-list/src/components/ShoppingList.tsx
+++ shopping-list/src/components/ShoppingList.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import { JsxAttribute } from 'typescript'
 
-export default function ShoppingList(): JSX.Element {
-  const items = [
-    { id: 1, product: "Lemon", quantity: 3 },
-    { id: 2, product: "Chicken", quantity: 2 },
-  ];
+// Having the interface here is okay. For larger applications
+// It would be wiser to put it in another file for reusability
+interface Item {
+  id: number;
+  product: string;
+  quantity: number;
+}
+// We take the Item interface and put it in our ShoppingListProps
+// interface and then use it as props
+interface ShoppingListProps {
+  items: Item[];
+}
+
+export default function ShoppingList(props: ShoppingListProps): JSX.Element {
+
 
   return (
     <div>
       <h1>Shopping List</h1>
       <ul>
         <li>
-          {items.map((item) => (
+          {props.items.map((item) => (
             <li key={item.id}>
               {item.product} - {item.quantity}
             </li>
