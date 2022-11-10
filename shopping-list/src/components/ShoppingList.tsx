c482682b83d9a841237fd041387cfa3dfe86501d
hammyhammer <hammyjacobs@gmail.com>
2022-11-10 10:26:02 -0500

    hardcoded data

--- shopping-list/src/components/ShoppingList.tsx
+++ shopping-list/src/components/ShoppingList.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { JsxAttribute } from 'typescript'
 
 export default function ShoppingList(): JSX.Element {
+  const items = [
+    { id: 1, product: "Lemon", quantity: 3 },
+    { id: 2, product: "Chicken", quantity: 2 },
+  ];
+
   return (
     <div>
       <h1>Shopping List</h1>
       <ul>
         <li>
-          Item One
+          {items.map((item) => (
+            <li key={item.id}>
+              {item.product} - {item.quantity}
+            </li>
+          ))}
         </li>
       </ul>
     </div>
