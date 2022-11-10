8228f04016b0569cb2fdde7345d8d564114acff7
hammyhammer <hammyjacobs@gmail.com>
2022-11-10 11:36:52 -0500

    can display new entries

--- shopping-list/src/App.tsx
+++ shopping-list/src/App.tsx
@@ -8,7 +8,10 @@ import ShoppingForm from './components/ShoppingForm';
 
 function App() {
   const [items, setItems] = useState<Item[]>([])
+  // This is not ideal. The id will be the same for all entries. we can use package
+  // to generate random ids. 
   const addItem = (product: string) => {
+    setItems([...items, { id: 3, product, quantity: 1 }])
     console.log("Success")
   }
   // const items = [
